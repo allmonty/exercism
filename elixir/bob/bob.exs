787102106379fc6adac2f88a20de54c81e3234a2
Allan <allanbrados@gmail.com>
2018-11-12 00:56:09 -0200

    elixir/bob pass test 12

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,10 +1,10 @@
 defmodule Bob do
   def hey(input) do
     cond do
+      nothing?(input) -> "Fine. Be that way!"
       yelling_question?(input) -> "Calm down, I know what I'm doing!"
       all_uppercase?(input) -> "Whoa, chill out!"
       question?(input) -> "Sure."
-      nothing?(input) -> "Fine. Be that way!"
       true -> "Whatever."
     end
   end
@@ -12,5 +12,5 @@ defmodule Bob do
   def all_uppercase?(input), do: String.match?(input, ~r/^[^a-z]+$/)
   def question?(input), do: String.match?(input, ~r/[?]$/)
   def yelling_question?(input), do: all_uppercase?(input) and question?(input)
-  def nothing?(input), do: input == ""
+  def nothing?(input), do: String.lstrip(input) == ""
 end
