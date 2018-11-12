5e8fddae7848c57db6d2cc6563b31899feacdd5e
Allan <allanbrados@gmail.com>
2018-11-12 00:39:02 -0200

    elixir/bob pass test 2

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,7 +1,10 @@
 defmodule Bob do
   def hey(input) do
     cond do
+      all_uppercase?(input) -> "Whoa, chill out!"
       true -> "Whatever."
     end
   end
+
+  def all_uppercase?(input), do: String.match?(input, ~r/^[^a-z]*$/)
 end
