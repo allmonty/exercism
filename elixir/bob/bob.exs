a90af9ec5d5119ff12230e147b64a808838c7c86
Allan <allanbrados@gmail.com>
2018-11-13 13:32:37 -0200

    elixir/bob fix functions should be private

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -9,15 +9,15 @@ defmodule Bob do
     end
   end
 
-  def all_uppercase?(input) do
+  defp all_uppercase?(input) do
     input
     |> String.replace(~r/[\d,? ]/, "")
     |> String.match?(~r/^[^a-z]+$/u)
   end
 
-  def question?(input), do: input =~ ~r/[?]$/
+  defp question?(input), do: input =~ ~r/[?]$/
 
-  def nothing?(input), do: String.trim_leading(input) == ""
+  defp nothing?(input), do: String.trim_leading(input) == ""
 
-  def yelling_question?(input), do: all_uppercase?(input) and question?(input)
+  defp yelling_question?(input), do: all_uppercase?(input) and question?(input)
 end
