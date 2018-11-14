405361335979ca2997d44e8e732523ef13586ed9
Allan <allanbrados@gmail.com>
2018-11-14 00:23:44 -0200

    elixir/rna-transcription pass tests

--- elixir/rna-transcription/rna_transcription.exs
+++ elixir/rna-transcription/rna_transcription.exs
@@ -9,5 +9,20 @@ defmodule RNATranscription do
   """
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
+    dna
+    |> Enum.reduce('', &(convert_to_rna(&1) ++ &2))
+    |> Enum.reverse()
+  end
+
+  @dna_to_rna %{
+    "A" => 'U',
+    "C" => 'G',
+    "T" => 'A',
+    "G" => 'C'
+  }
+
+  defp convert_to_rna(dna) do
+    dna = to_string([dna])
+    @dna_to_rna[dna]
   end
 end
