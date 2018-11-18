31b47c71197601f8ea309ca1c9a17b566414175b
Allan <allanbrados@gmail.com>
2018-11-18 03:22:28 -0200

    elixir/crypto-square pass

--- elixir/crypto-square/crypto_square.exs
+++ elixir/crypto-square/crypto_square.exs
@@ -7,10 +7,33 @@ defmodule CryptoSquare do
     "ac bd"
   """
   @spec encode(String.t()) :: String.t()
+  def encode(""), do: ""
+
   def encode(str) do
     str
     |> normalize()
+    |> crypto()
   end
 
-  defp normalize(str), do: str |> String.downcase() |> String.replace(~r/[^a-ẑ]/i, "")
+  defp normalize(str), do: str |> String.downcase() |> String.replace(~r/[^a-ẑ\d]/i, "")
+
+  defp crypto(str) do
+    {c, r} = calc_c_r(str)
+
+    str
+    |> String.pad_trailing(c * r)
+    |> String.graphemes()
+    |> Enum.chunk_every(c)
+    |> Enum.zip()
+    |> Enum.map_join(" ", fn x ->
+      x |> Tuple.to_list() |> Enum.join() |> String.trim()
+    end)
+  end
+
+  defp calc_c_r(str) do
+    side = str |> String.length() |> :math.sqrt()
+    c = side |> Float.ceil() |> round()
+    r = side |> Float.floor() |> round()
+    {c, r}
+  end
 end
