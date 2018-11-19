56e4bb05e0db3ea4b5cc9e7782c3fd95bd90c203
Allan <allanbrados@gmail.com>
2018-11-19 15:00:05 -0200

    elixir/meetup pass and refactor

--- elixir/meetup/meetup.exs
+++ elixir/meetup/meetup.exs
@@ -4,7 +4,6 @@ defmodule Meetup do
   """
 
   import Calendar.ISO, only: [days_in_month: 2, day_of_week: 3]
-  import Enum, only: [reverse: 1]
 
   @type weekday ::
           :monday
@@ -17,18 +16,6 @@ defmodule Meetup do
 
   @type schedule :: :first | :second | :third | :fourth | :last | :teenth
 
-  @teenths [13, 14, 15, 16, 17, 18, 19]
-
-  @parsed_weekdays %{
-    monday: 1,
-    tuesday: 2,
-    wednesday: 3,
-    thursday: 4,
-    friday: 5,
-    saturday: 6,
-    sunday: 7
-  }
-
   @doc """
   Calculate a meetup date.
 
@@ -40,22 +27,41 @@ defmodule Meetup do
     day =
       year
       |> days_by_weekday(month)
-      |> Map.get(@parsed_weekdays[weekday], 0)
-      |> reverse()
-      |> get_by_schedule(schedule)
+      |> get_by_weekday(weekday)
+      |> find_by_schedule(schedule)
 
     {year, month, day}
   end
 
   defp days_by_weekday(year, month) do
-    1..days_in_month(year, month)
+    days_in_month(year, month)..1
     |> Enum.reduce(%{}, fn day, acc ->
       day_of_week = day_of_week(year, month, day)
       Map.update(acc, day_of_week, [day], &[day | &1])
     end)
   end
 
-  defp get_by_schedule(days, :first), do: List.first(days)
-  defp get_by_schedule(days, :last), do: List.last(days)
-  defp get_by_schedule(days, :teenth), do: Enum.find(days, &(&1 in @teenths))
+  defp get_by_weekday(days_by_weekday, weekday),
+    do: Map.get(days_by_weekday, parsed_weekdays()[weekday], [])
+
+  defp find_by_schedule(days, :first), do: List.first(days)
+  defp find_by_schedule(days, :second), do: Enum.at(days, 1)
+  defp find_by_schedule(days, :third), do: Enum.at(days, 2)
+  defp find_by_schedule(days, :fourth), do: Enum.at(days, 3)
+  defp find_by_schedule(days, :last), do: List.last(days)
+  defp find_by_schedule(days, :teenth), do: Enum.find(days, &(&1 in teenths()))
+
+  defp teenths, do: [13, 14, 15, 16, 17, 18, 19]
+
+  defp parsed_weekdays do
+    %{
+      monday: 1,
+      tuesday: 2,
+      wednesday: 3,
+      thursday: 4,
+      friday: 5,
+      saturday: 6,
+      sunday: 7
+    }
+  end
 end
