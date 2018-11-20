e62e959a5f2118a5294f45720402ffaa6e12c106
Allan <allanbrados@gmail.com>
2018-11-20 02:09:03 -0200

    elixir/diamond refactor

--- elixir/diamond/diamond.exs
+++ elixir/diamond/diamond.exs
@@ -1,38 +1,29 @@
 defmodule Diamond do
+  import String, only: [duplicate: 2]
+
   @doc """
   Given a letter, it prints a diamond starting with 'A',
   with the supplied letter at the widest point.
   """
   @spec build_shape(char) :: String.t()
   def build_shape(letter) do
-    build(letter, letter, :any)
+    left_half = half(letter - 1, letter) |> Enum.reverse() |> Enum.join()
+    right_half = half(letter - 1, letter) |> Enum.join()
+    left_half <> line(letter, letter) <> right_half
   end
 
-  defp build(?A, letter, _) do
-    side_spaces = String.duplicate(" ", letter - ?A)
-    side_spaces <> "A" <> side_spaces <> "\n"
+  defp half(curr, _) when curr < ?A, do: []
+  defp half(curr, letter), do: [line(curr, letter) | half(curr - 1, letter)]
+
+  defp line(?A, letter), do: side_spaces(?A, letter) <> "A" <> side_spaces(?A, letter) <> "\n"
+
+  defp line(curr, letter) do
+    s_curr = [curr] |> to_string()
+    side_spaces = side_spaces(curr, letter)
+    middle_spaces = middle_spaces(curr, letter)
+    side_spaces <> s_curr <> middle_spaces <> s_curr <> side_spaces <> "\n"
   end
 
-  defp build(current, letter, _) when current == letter do
-    build(current - 1, letter, :left) <>
-      line(current, letter) <> build(current - 1, letter, :right)
-  end
-
-  defp build(current, letter, :left) do
-    build(current - 1, letter, :left) <> line(current, letter)
-  end
-
-  defp build(current, letter, :right) do
-    line(current, letter) <> build(current - 1, letter, :right)
-  end
-
-  defp line(current, letter) do
-    s_current = [current] |> to_string()
-    side_spaces = side_spaces(current, letter)
-    middle_spaces = middle_spaces(current, letter)
-    side_spaces <> s_current <> middle_spaces <> s_current <> side_spaces <> "\n"
-  end
-
-  defp middle_spaces(current, _), do: String.duplicate(" ", (current - ?A) * 2 - 1)
-  defp side_spaces(current, letter), do: String.duplicate(" ", letter - current)
+  defp middle_spaces(curr, _), do: duplicate(" ", (curr - ?A) * 2 - 1)
+  defp side_spaces(curr, letter), do: duplicate(" ", letter - curr)
 end
