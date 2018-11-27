1c0f08448279efbfa615dc83e455a4c901b4898b
Allan <allanbrados@gmail.com>
2018-11-27 20:27:54 -0200

    elixir/binary-search pass

--- elixir/binary-search/binary_search.exs
+++ elixir/binary-search/binary_search.exs
@@ -18,8 +18,20 @@ defmodule BinarySearch do
   """
 
   @spec search(tuple, integer) :: {:ok, integer} | :not_found
-  def search({}, _), do: :not_found
+  def search(numbers, key), do: do_search(numbers, key, 0, tuple_size(numbers) - 1)
 
-  def search(numbers, key) do
+  defp do_search({}, _, _, _), do: :not_found
+  defp do_search(_, _, left, right) when left > right, do: :not_found
+
+  defp do_search(numbers, key, left, right) do
+    mid = average(left, right)
+
+    case elem(numbers, mid) do
+      x when x > key -> do_search(numbers, key, left, mid - 1)
+      x when x < key -> do_search(numbers, key, mid + 1, right)
+      _ -> {:ok, mid}
+    end
   end
+
+  defp average(x, y), do: Integer.floor_div(x + y, 2)
 end
