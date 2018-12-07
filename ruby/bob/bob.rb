69105bc51ffc1a62ca2c5b1f33e2287af7798a59
Allan <allanbrados@gmail.com>
2018-12-07 13:25:20 -0200

    ruby/bob pass test 2..4

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,7 +1,11 @@
 class Bob
   def self.hey(text)
     case text
-    when /.*/
+    when /^[A-Z\s]+!*$/
+      "Whoa, chill out!"
+    when /\?$/
+      "Sure."
+    else
       "Whatever."
     end
   end
