175102f24f04ec5cfba6f5de66615d961090b0f7
Allan <allanbrados@gmail.com>
2018-12-28 13:27:13 -0200

    elixir/clock pass tests of create

--- elixir/clock/clock.exs
+++ elixir/clock/clock.exs
@@ -9,6 +9,12 @@ defmodule Clock do
   """
   @spec new(integer, integer) :: Clock
   def new(hour, minute) do
+    total_minutes = hour * 60 + minute
+
+    %Clock{
+      hour: total_minutes |> Integer.floor_div(60) |> Integer.mod(24),
+      minute: total_minutes |> Integer.mod(60)
+    }
   end
 
   @doc """
@@ -20,8 +26,16 @@ defmodule Clock do
   @spec add(Clock, integer) :: Clock
   def add(%Clock{hour: hour, minute: minute}, add_minute) do
   end
+
+  # ------------- ------------- ------------- #
 end
 
 defimpl String.Chars, for: Clock do
-  def to_string(clock), do: "#{clock.hour}:#{clock.minute}"
+  def to_string(clock) do
+    hour = clock.hour |> time_padding()
+    minute = clock.minute |> time_padding()
+    "#{hour}:#{minute}"
+  end
+
+  defp time_padding(x), do: x |> Kernel.to_string() |> String.pad_leading(2, "0")
 end
